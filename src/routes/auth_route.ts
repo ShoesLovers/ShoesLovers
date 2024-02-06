16b2b027e1f0bd791fdcb002c1a8dbc0c8025b7e
omerbenarieh <omerrbenarieh@gmail.com>
2024-02-06 23:59:46 +0200

    Start Swagger

--- src/routes/auth_route.ts
+++ src/routes/auth_route.ts
@@ -3,9 +3,149 @@ import AuthController from '../controllers/auth_controller'
 
 const router = express.Router()
 
+/**
+ * @swagger
+ * tags:
+ *  name: Auth
+ *  description: The Authentication API
+ */
+
+/**
+ * @swagger
+ * components:
+ *  securitySchemes:
+ *   bearerAuth:
+ *    type: http
+ *    scheme: bearer
+ *    bearerFormat: JWT
+ */
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *   Account:
+ *    type: object
+ *    required:
+ *     - email
+ *     - name
+ *     - password
+ *    properties:
+ *     email:
+ *      type: string
+ *      description: The user email
+ *     name:
+ *      type: string
+ *      description: The user name
+ *     password:
+ *      type: string
+ *      description: The user password
+ *    example:
+ *     email: 'bob@gmail.com'
+ *     name: 'Bob'
+ *     password: '123456'
+ */
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *   LoginSchema:
+ *    type: object
+ *    required:
+ *     - email
+ *     - password
+ *    properties:
+ *     email:
+ *      type: string
+ *      description: The user email
+ *     password:
+ *      type: string
+ *      description: The user password
+ *    example:
+ *     email: 'bob@gmail.com'
+ *     password: '123456'
+ */
+
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: registers a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Account'
+ *     responses:
+ *       200:
+ *         description: The new user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Account'
+ */
+
 router.post('/register', AuthController.register)
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Tokens:
+ *       type: object
+ *       required:
+ *         - accessToken
+ *         - refreshToken
+ *       properties:
+ *         accessToken:
+ *           type: string
+ *           description: The JWT access token
+ *         refreshToken:
+ *           type: string
+ *           description: The JWT refresh token
+ *       example:
+ *         accessToken: '123cd123x1xx1'
+ *         refreshToken: '134r2134cr1x3c'
+ */
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: registers a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/LoginSchema'
+ *     responses:
+ *       200:
+ *         description: The acess & refresh tokens
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Tokens'
+ */
 router.post('/login', AuthController.login)
+
+/**
+ * @swagger
+ * /auth/logout:
+ *   post:
+ *     summary: logout a user
+ *     tags: [Auth]
+ *     description: need to provide the refresh token in the auth header
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: logout completed successfully
+ */
 
 router.post('/logout', AuthController.logout)
 
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,27 @@
 import initApp from './app'
-
-console.log('You are running the server in ' + process.env.NODE_ENV + 'mode')
+import swaggerJsDoc from 'swagger-jsdoc'
+import swaggerUI from 'swagger-ui-express'
 
 initApp().then(app => {
+  const options = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Social Media Network For Shoes Lovers REST API',
+        version: '1.0.0',
+        description: 'REST server including authentication using JWT',
+      },
+      servers: [{ url: 'http://localhost:3000' }],
+    },
+    apis: ['./src/routes/*.ts'],
+  }
+  const specs = swaggerJsDoc(options)
+  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs))
+
+  console.log('You are running the server in ' + process.env.NODE_ENV + 'mode')
+
   const port = process.env.PORT
   app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`ShoesLovers Web App listening at http://localhost:${port}`)
   })
 })
