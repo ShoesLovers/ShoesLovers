e71b6ba39cc0a0223a28348af39c1cb30811fca4
omerbenarieh <omerrbenarieh@gmail.com>
2024-02-16 02:14:40 +0200

    Adding generateTokens() function

--- src/tests/post.test.ts
+++ src/tests/post.test.ts
@@ -9,7 +9,6 @@ import {
   registerAccount,
   createAccountObject,
   createPostObject,
-  loginAccount,
   createPost,
   getPostById,
   getAllPosts,
@@ -30,7 +29,6 @@ afterAll(async () => {
 let app: Express
 let dbAccount: request.Response
 let dbPost: request.Response
-let login: request.Response
 let accessToken: string
 
 const post = createPostObject('test1', 'message1', [])
@@ -43,8 +41,7 @@ describe('Tests Posts', () => {
     await Post.deleteMany()
     await Account.deleteMany()
     dbAccount = await registerAccount(app, account)
-    login = await loginAccount(app, account)
-    accessToken = login.body.accessToken
+    accessToken = dbAccount.body.accessToken
   })
 
   test('Test if no posts', async () => {
@@ -57,7 +54,7 @@ describe('Tests Posts', () => {
     dbPost = await createPost(app, post, accessToken)
     expect(dbPost.status).toBe(201)
     expect(dbPost.body.title).toBe(post.title)
-    expect(dbPost.body.owner).toBe(dbAccount.body._id)
+    expect(dbPost.body.owner).toBe(dbAccount.body.account._id)
     expect(dbPost.body.comments).toEqual([])
     expect(dbPost.body.message).toBe(post.message)
   })
@@ -81,7 +78,7 @@ describe('Tests Posts', () => {
 
     expect(response.status).toBe(200)
     expect(response.body.title).toBe(post.title)
-    expect(response.body.owner._id).toBe(dbAccount.body._id)
+    expect(response.body.owner._id).toBe(dbAccount.body.account._id)
     expect(response.body.comments).toEqual([])
     expect(response.body.message).toBe(post.message)
   })
@@ -146,14 +143,14 @@ describe('Tests Posts', () => {
   })
 
   test('Test delete post', async () => {
-    console.log(dbAccount.body._id)
+    console.log(dbAccount.body.account._id)
     const response = await deletePost(app, dbPost.body._id, accessToken)
     expect(response.status).toBe(200)
     expect(response.text).toBe('Post deleted')
   })
 
   test('Test Post is not exist on user posts array', async () => {
-    const response = await Account.findOne({ _id: dbAccount.body._id })
+    const response = await Account.findOne({ _id: dbAccount.body.account._id })
     expect(response.posts.length).toBe(0)
   })
 
@@ -187,7 +184,7 @@ describe('Tests Posts', () => {
 
   test('Delete Account', async () => {
     const response = await request(app)
-      .delete(`/account/${dbAccount.body._id}`)
+      .delete(`/account/${dbAccount.body.account._id}`)
       .set('Authorization', `JWT ${accessToken}`)
     expect(response.status).toBe(200)
   })
