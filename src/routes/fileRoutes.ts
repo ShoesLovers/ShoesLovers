d43b15556decc4f0b02a2e05ad9b759927068930
omerbenarieh <omerrbenarieh@gmail.com>
2024-03-17 22:08:17 +0200

    bla

--- src/routes/fileRoutes.ts
+++ src/routes/fileRoutes.ts
@@ -3,7 +3,7 @@ const router = express.Router()
 import multer from 'multer'
 
 // const base = "http://" + process.env.DOMAIN_BASE + ":" + process.env.PORT + "/";
-const base = 'http://localhost:3000/'
+const base = 'https://193.106.55.189/'
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
