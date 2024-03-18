d6875a8fee2593539598adf0af29328c181ba6a7
DorSonego <dorsonego123@gmail.com>
2024-03-18 15:18:40 +0200

    sdfs

--- src/routes/fileRoutes.ts
+++ src/routes/fileRoutes.ts
@@ -1,28 +1,28 @@
-import express from 'express'
-const router = express.Router()
-import multer from 'multer'
+import express from 'express';
+const router = express.Router();
+import multer from 'multer';
 
 // const base = "http://" + process.env.DOMAIN_BASE + ":" + process.env.PORT + "/";
-let base: string
-if (process.env.NODE_ENV === 'development') {
-  base = 'http://localhost:3000/'
+let base: string;
+if (process.env.NODE_ENV.trim() === 'development') {
+  base = 'http://localhost:3000/';
 } else {
-  base = 'https://node29.cs.colman.ac.il/'
+  base = 'https://node29.cs.colman.ac.il/';
 }
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/')
+    cb(null, 'public/');
   },
   filename: function (req, file, cb) {
-    const ext = file.originalname.split('.').filter(Boolean).slice(1).join('.')
-    cb(null, Date.now() + '.' + ext)
+    const ext = file.originalname.split('.').filter(Boolean).slice(1).join('.');
+    cb(null, Date.now() + '.' + ext);
   },
-})
-const upload = multer({ storage: storage })
+});
+const upload = multer({ storage: storage });
 
 router.post('/', upload.single('file'), function (req, res) {
-  console.log('router.post(/file: ' + base + req.file.path)
-  res.status(200).send({ url: base + req.file.path })
-})
-export = router
+  console.log('router.post(/file: ' + base + req.file.path);
+  res.status(200).send({ url: base + req.file.path });
+});
+export = router;
